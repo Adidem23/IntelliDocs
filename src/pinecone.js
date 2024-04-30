30ec0963c0d23abd03d1f0198412a95c40da45e7
Aditya Suryawanshi <124609794+Adidem23@users.noreply.github.com>
2024-04-30 15:55:58 +0530

    Update pinecone.js

--- src/pinecone.js
+++ src/pinecone.js
@@ -1,6 +1,6 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
-const pinecone= new Pinecone({apiKey:'ce68792c-9c25-466b-998d-acbd26b2b015'})
+const pinecone= new Pinecone({apiKey:''})
 
 console.log('pinecone initialized')
 
